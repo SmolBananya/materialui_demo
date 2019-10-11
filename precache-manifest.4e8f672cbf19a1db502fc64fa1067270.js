4fd21293163646833c1851215a44c6bd11510773
Kaskela Ronja <s1900620@edu.bc.fi>
2019-10-11 09:55:07 +0300

    Updates

---
+++ precache-manifest.4e8f672cbf19a1db502fc64fa1067270.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d757939bde3c1e6f0d2273e6bf480f",
+    "url": "/materialui_demo/index.html"
+  },
+  {
+    "revision": "2fdd8f3b9e391bb827c4",
+    "url": "/materialui_demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c694fa901d8aad1ea7e0",
+    "url": "/materialui_demo/static/js/2.19dc37a4.chunk.js"
+  },
+  {
+    "revision": "2fdd8f3b9e391bb827c4",
+    "url": "/materialui_demo/static/js/main.e9dbe344.chunk.js"
+  },
+  {
+    "revision": "6d35d25333f998efc3aa",
+    "url": "/materialui_demo/static/js/runtime-main.66be0787.js"
+  }
+]);
